58d1cd7889bf4d3f377dc3c23bbd911757ae8ec8
Jacobdye99 <Jacobdye99@gmail.com>
2022-09-19 18:45:09 -0400

    Added Login and register

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -8,6 +8,8 @@ export default function Navbar() {
         <Link to={'/'} id="Logo" ><img src="https://github.com/Jacobdye99/DnDCC2/blob/main/Assets/dungeons-and-dragons-723167.png?raw=true" alt="DnD Logo" id="Logo" /><h3 className="CC">Character Creator</h3></Link>
         <Link to={'/Creator'} id="Creator">Creator</Link>
         <Link to={'/Characters'} id='Character'>Characters</Link>
+        <Link to={'/Login'} id="Login" >Login</Link>
+        <Link to={'/Register'} id="Register">Register</Link>
     </div>
   )
 }
