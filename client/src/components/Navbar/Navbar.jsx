49b49fe5f07fe6846d2e604e32f46d5f48ea93c6
Jacobdye99 <Jacobdye99@gmail.com>
2022-11-16 23:55:16 -0500

    Added Some styling to the Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -6,10 +6,10 @@ export default function Navbar() {
   return (
     <div className="Top">
         <Link to={'/'} id="Logo" ><img src="https://github.com/Jacobdye99/DnDCC2/blob/main/Assets/dungeons-and-dragons-723167.png?raw=true" alt="DnD Logo" id="Logo" /><h3 className="CC">Character Creator</h3></Link>
-        <Link to={'/Creator'} id="Creator">Creator</Link>
-        <Link to={'/Characters'} id='Character'>Characters</Link>
-        <Link to={'/Login'} id="Login" >Login</Link>
-        <Link to={'/Register'} id="Register">Register</Link>
+        <Link to={'/Creator'} id="Creator" className="Links">Creator</Link>
+        <Link to={'/Characters'} id='Character' className="Links">Characters</Link>
+        <Link to={'/Login'} id="Login" className="Links">Login</Link>
+        <Link to={'/Register'} id="Register" className="Links">Register</Link>
     </div>
   )
 }
